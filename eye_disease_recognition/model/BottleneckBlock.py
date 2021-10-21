4f26c21a0785e20f63fc49f42bdddca335f94fff
SeaElm <yrz1016@163.com>
2021-10-21 22:03:07 +0800

    modified model

--- eye_disease_recognition/model/BottleneckBlock.py
+++ eye_disease_recognition/model/BottleneckBlock.py
@@ -2,7 +2,6 @@ import paddle.nn as nn
 import paddle
 
 from .ConvBNLayer import ConvBNLayer
-
 # 定义残差块
 # 每个残差块会对输入图片做三次卷积，然后跟输入图片进行短接
 # 如果残差块中第三次卷积输出特征图的形状和输入不一致，则对输入图片做1x1卷积，将其输出形状调整为一致
@@ -17,6 +16,7 @@ class BottleneckBlock(nn.Layer):
         super(BottleneckBlock,self).__init__()
         pathA_dict={}
         pathB_dict={}
+        # default版本
         pathA_default=nn.Sequential(
             ConvBNLayer(num_channels=num_channels,num_filters=num_filters,filter_size=1,stride=stride,),
             ConvBNLayer(num_channels=num_filters,num_filters=num_filters,filter_size=3,),
@@ -25,6 +25,20 @@ class BottleneckBlock(nn.Layer):
         pathB_default=nn.Sequential(
             ConvBNLayer(num_channels=num_channels,num_filters=num_filters*4,filter_size=1,stride=stride,act='None'),
         )
+        # B版本修改
+        pathA_tweak=nn.Sequential(
+            ConvBNLayer(num_channels=num_channels,num_filters=num_filters,filter_size=1,),
+            ConvBNLayer(num_channels=num_filters,num_filters=num_filters,filter_size=3,stride=stride,),
+            ConvBNLayer(num_channels=num_filters,num_filters=num_filters*4,filter_size=1,),
+        )
+        pathA_dict['B']=pathA_tweak
+        # D 版本修改
+        pathB_tweak=nn.Sequential(
+            nn.AvgPool2D(kernel_size=stride,stride=stride),
+            ConvBNLayer(num_channels=num_channels,num_filters=num_filters*4,filter_size=1),
+        )
+        pathB_dict['D']=pathB_tweak
+        pathA_dict['D']=pathA_tweak
         self.shortcut=shortcut
         self.pathA=pathA_dict.get(version,pathA_default)
         self.pathB=pathB_dict.get(version,pathB_default)
--- eye_disease_recognition/model/ResNet.py
+++ eye_disease_recognition/model/ResNet.py
@@ -4,6 +4,7 @@ import paddle
 from .ConvBNLayer import ConvBNLayer
 from .BottleneckBlock import BottleneckBlock
 
+
 # 定义ResNet模型
 class ResNet(nn.Layer):
     def __init__(self, layers=50, class_dim=10, version='O'):
@@ -31,12 +32,21 @@ class ResNet(nn.Layer):
         # stage1-4所使用残差块的输出通道数
         num_filters = [64, 128, 256, 512]
 
-        # input stem模块,默认版本：64个7x7的卷积加上一个3x3最大化池化层，步长均为2
+        # input stem模块,default版本：64个7x7的卷积加上一个3x3最大化池化层，步长均为2
         input_stem_dict = {}
         input_stem_default = nn.Sequential(
             ConvBNLayer(num_channels=3, num_filters=64, filter_size=7, stride=2, ),
             nn.MaxPool2D(kernel_size=3, stride=2, padding=1, ),
         )
+        # C版本修改
+        input_stem_tweak = nn.Sequential(
+            ConvBNLayer(num_channels=3, num_filters=64, filter_size=3, stride=2, ),
+            ConvBNLayer(num_channels=64, num_filters=64, filter_size=3, ),
+            ConvBNLayer(num_channels=64, num_filters=64, filter_size=3, ),
+            nn.MaxPool2D(kernel_size=3, stride=2, padding=1, ),
+        )
+        input_stem_dict['C'] = input_stem_tweak
+
         self.input_stem = input_stem_dict.get(version, input_stem_default)
 
         # stage1-4模块，使用各个残差块进行卷积操作
